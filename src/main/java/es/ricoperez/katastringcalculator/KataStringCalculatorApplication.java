11b2e1fb400e6692fba71998f552f7e920479206
Óscar Jesús López Sierra <oscar@ricoperez.es>
2023-06-21 08:07:39 +0200

    Initial commit

---
+++ src/main/java/es/ricoperez/katastringcalculator/KataStringCalculatorApplication.java
@@ -0,0 +1,13 @@
+package es.ricoperez.katastringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataStringCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataStringCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ricoperez/katastringcalculator/KataStringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ricoperez.katastringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataStringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
